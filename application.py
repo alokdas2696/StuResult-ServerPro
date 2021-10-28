080a62dd180b9f457dee8e3dce4e4069065fdd45
Your Name <you@example.com>
2021-10-28 22:42:12 +0530

    19commit

--- application.py
+++ application.py
@@ -21,11 +21,11 @@ application.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(application)
 
 application.config['MAIL_SERVER'] = 'smtp.gmail.com'
-application.config['MAIL_PORT'] = 587
-application.config['MAIL_USERNAME'] = os.environ.get('username')
-application.config['MAIL_PASSWORD'] = os.environ.get('password')
-application.config['MAIL_USE_TLS'] = True
-application.config['MAIL_USE_SSL'] = False
+application.config['MAIL_PORT'] = 465
+application.config['MAIL_USERNAME'] = params['gmail-user']
+application.config['MAIL_PASSWORD'] = params['gmail-password']
+application.config['MAIL_USE_TLS'] = False
+application.config['MAIL_USE_SSL'] = True
 
 mail = Mail(application)
 
